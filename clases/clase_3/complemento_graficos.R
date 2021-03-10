b69ff17b195aaf11da31baece43455d8c0a4522f
andy <cursos@a2b2c.org>
2021-03-10 18:16:27 -0300

    Agrega points a complemento_graficos.R

--- clases/clase_3/complemento_graficos.R
+++ clases/clase_3/complemento_graficos.R
@@ -1,7 +1,19 @@
 iris
 boxplot(iris[, 1:4])
 boxplot(iris$Sepal.Length ~ iris$Species)
-plot(iris$Sepal.Length, iris$Sepal.Width, main = "Sepal.Length vs. Sepal.Width", 
+setosa <- iris[iris$Species == "setosa", ]
+versicolor <- iris[iris$Species == "versicolor", ]
+virginica <- iris[iris$Species == "virginica", ]
+summary(iris$Sepal.Length)
+summary(iris$Sepal.Width)
+plot(setosa$Sepal.Length, setosa$Sepal.Width, main = "Setosa", 
+     xlab = "Sepal Length", ylab="Sepal Width", xlim=c(4, 8), ylim=c(1, 5),
+     col = "black")
+points(versicolor$Sepal.Length, versicolor$Sepal.Width, col = "red")
+points(virginica$Sepal.Length, virginica$Sepal.Width, col = "green")
+legend("topleft", legend = c("Setosa", "Versicolor", "Virginica"), col = c("black", "red", "green"), pch=1, bty = "n")
+
+plot(iris$Sepal.Length, iris$Sepal.Width, main = "Iris", 
      xlab = "Sepal Length", ylab="Sepal Width", xlim=c(4, 8), ylim=c(1, 5),
      col = iris$Species)
 legend("topleft", legend = c("Setosa", "Versicolor", "Virginica"), col = 1:3, pch=1, bty = "n")
